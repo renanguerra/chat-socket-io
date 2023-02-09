1af13e606afd2c6290bbe03ca01ce1bc22c9b40c
Renan Guerra <renanguerra@MacBook-Air-de-Renan.local>
2023-02-09 13:00:25 -0300

    added hello world get route

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// app.get(`/`, (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
+app.get(`/`, (req, res) => {
+  res.send("<h1>Olá mundo!</h1>");
+});
 
 server.listen(3000, () => console.log(`listening port 3000`));
 
